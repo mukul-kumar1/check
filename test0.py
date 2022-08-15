b3201a0e49821407bd59149f47b9880d5b68fdc7
mukul-kumar1 <108761874+mukul-kumar1@users.noreply.github.com>
2022-08-15 15:18:48 +0530

    update 2
    placement

--- test0.py
+++ test0.py
@@ -1,3 +1,5 @@
 print('Mukul')
 
 My name is Mukul Kumar. CHeck check. This is a paragraph written to test the typing speed of the concerenc person. Please bear with me if there is any mistake in the 
+
+Now, this is high students start preparing for placements
