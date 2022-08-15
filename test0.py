509d34c17d2694473ac3ea1958bf58b082a02014
mukul-kumar1 <108761874+mukul-kumar1@users.noreply.github.com>
2022-08-15 15:50:47 +0530

    Delete test0.py

--- test0.py
+++
@@ -1,5 +0,0 @@
-print('Mukul')
-
-My name is Mukul Kumar. CHeck check. This is a paragraph written to test the typing speed of the concerenc person. Please bear with me if there is any mistake in the 
-
-Now, this is high students start preparing for placements
